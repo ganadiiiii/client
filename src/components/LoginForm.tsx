7ae29a3cd06bca7d4e4199ba208b5ee1c38f36f4
hyeran1216 <hran1216@khu.ac.kr>
2025-08-17 21:22:19 +0900

    refactor: styles from CSS to Tailwind CSS

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 const LoginForm: React.FC = () => {
   const navigate = useNavigate();
@@ -28,13 +28,7 @@ const LoginForm: React.FC = () => {
           </div>
           
           {/* 로그인 제목 */}
-          <h1 className="px-8 text-nowrap" style={{
-            color: '#C69FD4',
-            fontFamily: 'BagelFatOne-Regular',
-            fontSize: '36px',
-            fontWeight: '400',
-            lineHeight: 'normal'
-          }}>
+          <h1 className="px-8 text-[36px] text-nowrap text-[#C69FD4] font-normal" style={{ fontFamily: 'BagelFatOne-Regular' }}>
             CUSTOMER LOGIN
           </h1>
           
@@ -88,57 +82,33 @@ const LoginForm: React.FC = () => {
           <div className="flex gap-4 pt-4">
             {/* Sign in 버튼 */}
             <button
-              type="button"
-              className="flex-1 py-4 rounded hover:bg-gray-300 transition-colors"
-              style={{
-                paddingLeft: '50px',
-                paddingRight: '50px',
-                backgroundColor: '#EDEDED',
-                color: '#000000',
-                fontFamily: 'NEXON Lv1 Gothic OTF',
-                fontSize: '24px',
-                fontWeight: '700',
-                lineHeight: 'normal'
-              }}
-            >
+                type="button"
+                className="flex-1 py-4 pl-[50px] pr-[50px] rounded bg-[#EDEDED] text-black text-[24px] font-bold hover:bg-gray-300 transition-colors"
+                style={{ fontFamily: 'NEXON Lv1 Gothic OTF' }}
+              >
               Sign in
             </button>
             
             {/* Create Account 버튼 */}
             <button
-              type="button"
-              onClick={() => navigate('/signup')}
-              className="flex-1 py-4 rounded hover:bg-pink-300 transition-colors"
-              style={{
-                paddingLeft: '50px',
-                paddingRight: '50px',
-                backgroundColor: '#FFD1D4',
-                color: '#000000',
-                fontFamily: 'NEXON Lv1 Gothic OTF',
-                fontSize: '24px',
-                fontWeight: '700',
-                lineHeight: 'normal'
-              }}
-            >
+                type="button"
+                onClick={() => navigate('/signup')}
+                className="flex-1 py-4 pl-[50px] pr-[50px] rounded bg-[#FFD1D4] text-black text-[24px] font-bold hover:bg-pink-300 transition-colors"
+                style={{ fontFamily: 'NEXON Lv1 Gothic OTF' }}
+              >
               Create Account
             </button>
           </div>
           
           {/* Reset password 링크 */}
           <div className="text-center pt-4">
-            <a 
-              href="#" 
-              className="hover:text-gray-700 transition-colors"
-              style={{
-                color: '#999999',
-                fontFamily: 'NEXON Lv1 Gothic OTF',
-                fontSize: '24px',
-                fontWeight: '300',
-                lineHeight: 'normal'
-              }}
-            >
-              Forgot your password
-            </a>
+          <Link
+            to="/forgot-password"
+            className="hover:text-gray-700 transition-colors text-[24px] font-light text-[#999999]"
+            style={{ fontFamily: 'NEXON Lv1 Gothic OTF' }}
+          >
+            Forgot your password
+          </Link>
           </div>
           </form>
         </div>

3e6f48370a1a4cebd4ebd01e4aead4407b2b187b
hyeran1216 <hran1216@khu.ac.kr>
2025-08-18 16:49:47 +0900

    feat: add routing for customizing

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,39 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
+import CustomizingPage from './pages/CustomizingPage';
 
 
 function App() {
   return (
     <Router>
-      <div className="bg-[#FCFBF6] min-h-[1800px]">
-        <Navbar />
-        <main className="pt-[579px]">
-          <Routes>
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/signup" element={<SignUpForm />} />
-          </Routes>
-        </main>
-        <footer className="w-full h-[200px] bg-[#EDEDED]"></footer>
-      </div>
+      <Routes>
+        <Route 
+          path="/customizing" 
+          element={
+            <div>
+              <Navbar />
+              <CustomizingPage />
+            </div>
+          } 
+        />
+        <Route 
+          path="/*" 
+          element={
+            <div className="bg-[#FCFBF6] min-h-[1800px]">
+              <Navbar />
+              <main className="pt-[579px]">
+                <Routes>
+                  <Route path="/" element={<LoginForm />} />
+                  <Route path="/login" element={<LoginForm />} />
+                  <Route path="/signup" element={<SignUpForm />} />
+                </Routes>
+              </main>
+              <footer className="w-full h-[200px] bg-[#EDEDED]"></footer>
+            </div>
+          } 
+        />
+      </Routes>
     </Router>
   );
 }

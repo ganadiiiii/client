86da1560b0de21be076b8eac0f5046091063cd97
hyeran1216 <hran1216@khu.ac.kr>
2025-08-18 16:51:53 +0900

    feat: add option component

---
+++ src/components/FlowerSizeOptionCard.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+
+interface FlowerSizeOptionCardProps {
+  number: number;
+  size: string;
+  description: string;
+  flowerImg: string;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const dotColors = ['#F7DFDD', '#FEFBC6', '#D1DCE2'];
+
+const FlowerSizeOptionCard: React.FC<FlowerSizeOptionCardProps> = ({
+  number,
+  size,
+  description,
+  flowerImg,
+  isSelected,
+  onClick
+}) => {
+    return (
+        <div
+        className= "relative w-[335px] h-[431px] cursor-pointer transition-all duration-200"
+          onClick={onClick}
+          style={{
+            filter: isSelected ? 'none' : 'none',
+          }}
+        >
+            {/* 카드 배경 */}
+            <div 
+              className={`absolute inset-0 rounded-[21px] shadow-sm z-0 ${
+                isSelected ? 'bg-[#FFD1D4]' : 'bg-white'
+              }`}
+              style={{
+                boxShadow: '1.9px 1.9px 3.2px 0px rgba(0, 0, 0, 0.1)'
+              }}
+            />
+
+            {/* 그라데이션 영역 */}
+            <div 
+              className="absolute left-[20px] top-[23px] w-[297px] h-[329px] rounded-[27px] z-10"
+              style={{
+                backgroundImage: "url('./src/assets/generate/size.svg')"
+              }}
+            />
+
+            {/* 꽃 이미지 */}
+            <img
+                src={flowerImg}
+                alt={description}
+                className="absolute left-[49px] top-[52px] w-[227px] h-[272px] object-cover rounded-sm z-30"
+            />
+
+            {/* 원들 표시 */}
+            <div className="absolute top-[24px] left-[34px] flex gap-[13px] z-20">
+              {Array.from({ length: number }).map((_, idx) => (
+                <div
+                  key={idx}
+                  className="w-[18px] h-[18px] rounded-full"
+                  style={{
+                    backgroundColor: dotColors[idx],
+                  }}
+                />
+              ))}
+            </div>
+            
+            {/* 사이즈 */}
+            <div 
+              className="absolute left-[31px] top-[376px] text-black z-20"
+              style={{
+                fontFamily: 'Pretendard',
+                fontSize: '14px',
+                fontWeight: '400',
+                lineHeight: '1.18',
+                letterSpacing: '6%'
+              }}
+            >
+              {size}
+            </div>
+            
+            {/* 점선 */}
+            <div 
+              className="absolute left-[29px] top-[365px] w-[286px] h-0 border-t border-dashed border-[#D9D9D9] z-20"
+              style={{
+                borderWidth: '1.07px'
+              }}
+            />
+            
+            {/* 설명 */}
+            <div 
+              className="absolute right-[17px] top-[376px] text-black text-right z-20"
+              style={{
+                fontFamily: 'Pretendard',
+                fontSize: '14px',
+                fontWeight: '400',
+                lineHeight: '1.18',
+                letterSpacing: '6%'
+              }}
+            >
+              {description}
+            </div>
+            {/* 점선 */}
+            <div 
+              className="absolute left-[29px] top-[405px] w-[286px] h-0 border-t border-dashed border-[#D9D9D9] z-20"
+              style={{
+                borderWidth: '1.07px'
+              }}
+            />
+        </div>
+      );
+    };
+    
+    export default FlowerSizeOptionCard;
---
+++ src/components/OptionButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+interface OptionButtonProps {
+  option: string;
+  isSelected: boolean;
+  onClick: () => void;
+}
+
+const OptionButton: React.FC<OptionButtonProps> = ({ option, isSelected, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-8 py-4 rounded-full border transition-all duration-200 min-w-[144px] h-[61px] flex items-center justify-center ${
+        isSelected
+          ? 'bg-[#FFD1D4] border-[#868686]'
+          : 'bg-white border-[#868686] hover:bg-gray-50'
+      }`}
+      style={{
+        fontFamily: 'NEXON Lv1 Gothic OTF',
+        fontSize: '24px',
+        fontWeight: isSelected ? '700' : '400',
+        lineHeight: '1.15',
+        color: '#000000'
+      }}
+    >
+      {option}
+    </button>
+  );
+};
+
+export default OptionButton;

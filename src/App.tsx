77f45ff1c39e218fae327ba48dc45cdd00860d89
hyeran1216 <hran1216@khu.ac.kr>
2025-08-21 01:33:08 +0900

    refactor: refactor routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,28 @@ import Navbar from './components/Navbar';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
 import CustomizingPage from './pages/CustomizingPage';
+import CustomizingResultPage from './pages/CustomizingResultPage';
 
+
+function AppContent() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/customizing" element={<CustomizingPage />} />
+        <Route path="/customizing/result" element={<CustomizingResultPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        {/* <Route path="/" element={<LoginForm />} /> */}
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route 
-          path="/customizing" 
-          element={
-            <div>
-              <Navbar />
-              <CustomizingPage />
-            </div>
-          } 
-        />
-        <Route 
-          path="/*" 
-          element={
-            <div className="bg-[#FCFBF6] min-h-[1800px]">
-              <Navbar />
-              <main className="pt-[579px]">
-                <Routes>
-                  <Route path="/" element={<LoginForm />} />
-                  <Route path="/login" element={<LoginForm />} />
-                  <Route path="/signup" element={<SignUpForm />} />
-                </Routes>
-              </main>
-              <footer className="w-full h-[200px] bg-[#EDEDED]"></footer>
-            </div>
-          } 
-        />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }

d9d5aec542e65beecde88a8ea208c98c266331fa
hyeran1216 <hran1216@khu.ac.kr>
2025-08-28 22:38:50 +0900

    design: change button icon

--- src/app/pages/CustomizingResultPage.tsx
+++ src/app/pages/CustomizingResultPage.tsx
@@ -82,38 +82,18 @@ const CustomizingResultPage: React.FC = () => {
 
 						{/* 다운로드 버튼 */}
 						<button className="absolute -left-16 top-3/4 -translate-y-1/2 w-11 h-11 bg-[#FFDE87] rounded-full flex items-center justify-center hover:scale-105 transition-all duration-200">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="w-6 h-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-								/>
-							</svg>
+							<img
+								src="/src/assets/generate/share.svg"
+								alt="Left Button"
+							/>
 						</button>
 
 						{/* 업로드 버튼 */}
 						<button className="absolute -right-16 top-3/4 -translate-y-1/2 w-11 h-11 bg-[#FFDE87] rounded-full flex items-center justify-center hover:scale-105 transition-all duration-200">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								fill="none"
-								viewBox="0 0 24 24"
-								strokeWidth={1.5}
-								stroke="currentColor"
-								className="w-6 h-6"
-							>
-								<path
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-								/>
-							</svg>
+							<img
+								src="/src/assets/generate/save.svg"
+								alt="Right Button"
+							/>
 						</button>
 					</div>
 					<button

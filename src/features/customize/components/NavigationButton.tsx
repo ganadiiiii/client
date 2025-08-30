16bd74643ab955c4da58e8a9f9f99085dabc5e3e
hyeran1216 <144123922+hyeran1216@users.noreply.github.com>
2025-08-30 21:06:33 +0900

    design: navigation button

--- src/features/customize/components/NavigationButton.tsx
+++ src/features/customize/components/NavigationButton.tsx
@@ -21,15 +21,90 @@ const NavigationButton: React.FC<NavigationButtonProps> = ({
 			className={`w-[68px] h-[68px] relative rounded-full flex items-center justify-center ${className} ${disabled ? "" : "transition-all duration-100 hover:scale-95 cursor-pointer"}`}
 		>
 			{/* Pink background for enabled state, only for right button */}
-			{direction === "right" && (
-				<div
-					className={`absolute inset-0 rounded-full bg-[#FF9BAF] z-0 transition-opacity duration-300 ${!disabled ? "opacity-100" : "opacity-0"}`}
-				/>
+			{direction === "right" && !disabled && (
+				<div className="absolute inset-0 rounded-full bg-[#FF9BAF] z-0 transition-colors" />
 			)}
 
-			{/* Circular background with gradient and shadow effects */}
-			<div className="absolute inset-0 rounded-full shadow-md z-10 transition-colors duration-300 drop-shadow-[0px 4px 7.7px rgba(0, 0, 0, 0.15)] bg-linear-135 from-white/10 to-white/100 hover:from-white/10 hover:to-white/80" />
-
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="85"
+				height="85"
+				viewBox="0 0 85 85"
+				fill="none"
+				className="absolute top-9/16 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 pointer-events-none"
+			>
+				<g filter="url(#filter0_di)">
+					<circle
+						cx="42.1913"
+						cy="38.1913"
+						r="34.1913"
+						fill="url(#paint0_linear)"
+						shapeRendering="crispEdges"
+					/>
+				</g>
+				<defs>
+					<filter
+						id="filter0_di"
+						x="0.3"
+						y="0.3"
+						width="83.7828"
+						height="83.7828"
+						filterUnits="userSpaceOnUse"
+					>
+						<feFlood floodOpacity="0" result="BackgroundImageFix" />
+						<feColorMatrix
+							in="SourceAlpha"
+							type="matrix"
+							values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+							result="hardAlpha"
+						/>
+						<feOffset dy="4" />
+						<feGaussianBlur stdDeviation="3.85" />
+						<feComposite in2="hardAlpha" operator="out" />
+						<feColorMatrix
+							type="matrix"
+							values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"
+						/>
+						<feBlend
+							mode="normal"
+							in2="BackgroundImageFix"
+							result="effect1_dropShadow"
+						/>
+						<feBlend
+							mode="normal"
+							in="SourceGraphic"
+							in2="effect1_dropShadow"
+							result="shape"
+						/>
+						<feColorMatrix
+							in="SourceAlpha"
+							type="matrix"
+							values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+							result="hardAlpha"
+						/>
+						<feOffset dx="3" dy="4" />
+						<feGaussianBlur stdDeviation="0.8" />
+						<feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
+						<feColorMatrix
+							type="matrix"
+							values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.51 0"
+						/>
+						<feBlend mode="normal" in2="shape" result="effect2_innerShadow" />
+					</filter>
+					<linearGradient
+						id="paint0_linear"
+						x1="25.7054"
+						y1="12.546"
+						x2="45.2746"
+						y2="45.002"
+						gradientUnits="userSpaceOnUse"
+					>
+						<stop stopColor="white" stopOpacity="0.1" />
+						<stop offset="1" stopColor="white" stopOpacity="0.8" />
+					</linearGradient>
+				</defs>
+			</svg>
+			
 			{/* Arrow icon */}
 			{direction === "left" ? (
 				<svg

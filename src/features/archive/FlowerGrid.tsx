4c02337e226ed8b82ed4e39cec5ad6c7885d8314
hyeran1216 <hran1216@khu.ac.kr>
2025-09-11 03:23:43 +0900

    feat: grid translateY + rotate

--- src/features/archive/FlowerGrid.tsx
+++ src/features/archive/FlowerGrid.tsx
@@ -1,28 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import PageButton from "./components/PageButton";
 import { useState } from "react";
+import PageButton from "./components/PageButton";
+import FlowerCard from "./FlowerCard";
 
-const FlowerCard = ({ flower }: { flower: { id: number; name: string } }) => {
-	const navigate = useNavigate();
+// const FlowerCard = ({ flower }: { flower: { id: number; name: string } }) => {
+// 	const navigate = useNavigate();
 
-	return (
-		<div
-			className="h-[140px] aspect-[4/5] bg-white/60 hover:bg-white rounded-lg flex flex-col items-center justify-center border-2 border-[#FFD1D4] shadow-md p-2 hover:shadow-xl hover:border-[#FF6E77] transition-all cursor-pointer"
-			onClick={() => {
-				navigate("/customizing/card");
-			}}
-		>
-			<p className="text-sm font-bold text-[#FF6E77] text-center">
-				{flower.name}
-			</p>
-		</div>
-	);
-};
+// 	return (
+// 		<div
+// 			className="h-[140px] aspect-[4/5] bg-white/60 hover:bg-white rounded-lg flex flex-col items-center justify-center border-2 border-[#FFD1D4] shadow-md p-2 hover:shadow-xl hover:border-[#FF6E77] transition-all cursor-pointer"
+// 			onClick={() => {
+// 				navigate("/customizing/card");
+// 			}}
+// 		>
+// 			<p className="text-sm font-bold text-[#FF6E77] text-center">
+// 				{flower.name}
+// 			</p>
+// 		</div>
+// 	);
+// };
 
 const mockFlowerData = Array.from({ length: 50 }, (_, i) => ({
 	id: i + 1,
-	name: `꽃 #${i + 1}`,
+	url: `/src/assets/generate/size-3.png`,
 }));
+
 
 export default function FlowerGrid() {
 	const [currentPage, setCurrentPage] = useState(0);
@@ -45,9 +46,22 @@ export default function FlowerGrid() {
 	return (
 		<div className="absolute top-1/2 left-1/2 w-full max-w-3xl px-14 pt-46 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-4">
 			{/* 5x3 그리드 */}
-			<div className="flex-1 h-full mx-4 grid grid-cols-5 grid-rows-3 gap-x-6 gap-y-4 w-full">
-				{currentCards.map((card) => (
-					<FlowerCard key={card.id} flower={card} />
+			<div className="flex flex-col gap-12 items-center">
+				{[0, 1, 2].map((rowIdx) => (
+					<div
+						key={rowIdx}
+						className="grid grid-cols-5 gap-6"
+					>
+						{currentCards.slice(rowIdx * 5, rowIdx * 5 + 5).map((card, i) => (
+							<FlowerCard
+								key={card.id}
+								flower={card}
+								style={{
+									transform: `translateY(${[-14, -3, 0, -3, -14][i]}px) rotate(${[10.559, 3.791, 0, -3.791, -10.559][i]}deg)`,
+								}}
+							/>
+						))}
+					</div>
 				))}
 			</div>
 

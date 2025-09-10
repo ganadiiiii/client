438af076a7bd7cec07d4a4e44bd45f7c112c5c59
hyeran1216 <hran1216@khu.ac.kr>
2025-09-11 03:23:12 +0900

    refactor: responsive custom page

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,14 @@ export default {
 				},
 			},
 		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1440px',
+			'3xl': '1728px', // 커스텀 추가
+		  },
 	},
 	plugins: [],
 };

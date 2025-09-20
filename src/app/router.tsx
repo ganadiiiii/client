8feb209f7f107596c828d7c261ce206ba3fee830
hyeran1216 <hran1216@khu.ac.kr>
2025-09-21 06:32:45 +0900

    feat: add router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,12 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import LoginForm from "../features/login/components/LoginForm";
 import SignUpForm from "../features/login/components/SignUpForm";
 import ArchivePage from "./pages/ArchivePage";
-import CustomizingCardPage from "./pages/CustomizingCardPage";
 import CustomizingPage from "./pages/CustomizingPage";
 import CustomizingResultPage from "./pages/CustomizingResultPage";
 import ResultSentPage from "./pages/ResultSentPage";
 import OrderPage from "./pages/OrderPage";
 import FlowerInfoPage from "./pages/FlowerInfoPage";
+import DeliveryPage from "./pages/DeliveryPage";
+import PickupPage from "./pages/PickupPage";
 
 export const AppRoutes = () => {
 	return (
@@ -17,10 +18,11 @@ export const AppRoutes = () => {
 			<Route path="/customizing/result/sent" element={<ResultSentPage />} />
 			<Route path="/archive" element={<ArchivePage />} />
 			<Route path="/archive/:flowerId" element={<FlowerInfoPage />} />
-			<Route path="/customizing/card" element={<CustomizingCardPage />} />
 			<Route path="/login" element={<LoginForm />} />
 			<Route path="/signup" element={<SignUpForm />} />
 			<Route path="/shop" element={<OrderPage />} />
+			<Route path="/shop/delivery" element={<DeliveryPage />} />
+			<Route path="/shop/pickup" element={<PickupPage />} />
 		</Routes>
 	);
 };

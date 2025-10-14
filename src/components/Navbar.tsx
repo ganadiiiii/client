1759d3133053454123e50f5898c563ead986557f
hyeran1216 <hran1216@khu.ac.kr>
2025-10-14 14:01:05 +0900

    design: change nav bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,10 +22,20 @@ const Navbar: React.FC = () => {
 		<nav>
 			{/* 배경 이미지 영역: 로그인/회원가입/메인/온보딩에서만 보이도록 */}
 			{isAuthPage && (
-				<div
-					className="absolute top-0 left-0 w-screen h-[482px] 2xl:h-[579px] z-0 bg-cover bg-center bg-no-repeat"
-					style={{ backgroundImage: "url('./src/assets/nav_bg.svg')" }}
-				/>
+				<>
+					<div
+						className="absolute top-0 left-0 w-screen h-[357px] 2xl:h-[481px] z-0 bg-cover bg-center bg-no-repeat"
+						style={{ backgroundImage: "url('./src/assets/nav_bg.png')" }}
+					/>
+					<div
+						className="absolute top-[357px] 2xl:top-[481px] w-screen z-0"
+						style={{
+							height: "2em",
+							background:
+								"linear-gradient(90deg, #FFA8B5 0%, #E3CAEB 37.98%, #F8CBD6 68.27%, #FFBBD8 100%)",
+						}}
+					/>
+				</>
 			)}
 
 			{/* 상단 바만 배경색이 변경되도록 분리 */}

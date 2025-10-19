c1b0f95762c54ee286c1b201fe2e11803c9fd5c7
Jeongwon Kim <mickykim4@gmail.com>
2025-10-19 15:42:08 +0900

    minor design changes

--- src/features/login/components/Cart.tsx
+++ src/features/login/components/Cart.tsx
@@ -3,17 +3,17 @@ import { useNavigate } from "react-router-dom";
 import bg from "../../../assets/generate/bg.svg";
 
 const Cart: React.FC = () => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
 	return (
-        <div
+		<div
 			className="min-h-screen flex flex-col items-center justify-center relative bg-background bg-cover bg-center text-center"
 			style={{
 				backgroundImage: `url(${bg})`,
 			}}
-		>  
-            <div className="flex flex-col items-center justify-center">
-                <img src="/src/assets/cart.png" alt="chat" className="w-[10.8em] h-[13.2em] mb-9" />
+		>
+			<div className="flex flex-col items-center justify-center">
+				<img src="/src/assets/cart.png" alt="chat" className="w-[10.8em] h-[13.2em] mb-9" />
 				<p
 					className="text-black text-center font-semibold whitespace-nowrap text-3xl mb-14"
 					style={{
@@ -23,12 +23,12 @@ const Cart: React.FC = () => {
 				>
 					주문내역이 없어요!
 				</p>
-                <button className="py-4 px-32 rounded-full flex items-center justify-center bg-primary text-white text-lg" style={{ fontFamily: "NexonLv1Gothic" }} onClick={() => navigate("/archive")}>
-                    주문하러 가기
-                </button>
+				<button className="px-32 py-4.5 rounded-full text-lg font-semibold transition-all duration-200 bg-primary text-white hover:bg-primary/90 hover:scale-105 active:scale-95 cursor-pointer" style={{ fontFamily: "NexonLv1Gothic", fontWeight: "600", }} onClick={() => navigate("/archive")}>
+					주문하러 가기
+				</button>
 			</div>
-        </div>
-    );
+		</div>
+	);
 };
 
 export default Cart;

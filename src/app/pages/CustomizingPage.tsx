216c3ef62af8dced3a22a754d505198f482f9eac
hyeran1216 <hran1216@khu.ac.kr>
2025-10-25 12:15:25 +0900

    feat: 2.5s loading

--- src/app/pages/CustomizingPage.tsx
+++ src/app/pages/CustomizingPage.tsx
@@ -52,7 +52,10 @@ const CustomizingPage: React.FC = () => {
 		if (currentStep === 8 && currentAnswers.includes("None")) {
 			// 카드 생성 호출 후 결과 페이지로 이동
 			setIsLoading(true);
-			const result = await createCardFromAnswers({ answers, bouquetTitle });
+			const [result] = await Promise.all([
+				createCardFromAnswers({ answers, bouquetTitle }),
+				new Promise(resolve => setTimeout(resolve, 2500)) // 2.5초 대기
+			]);
 			setIsLoading(false);
 			if (result) {
 				navigate("/customizing/result", { 
